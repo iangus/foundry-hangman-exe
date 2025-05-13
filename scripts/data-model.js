964c6d1d70c1fc7de07ba9adb59af780292d89c6
iangus <iangus97@gmail.com>
2025-05-13 12:55:21 -0400

    Data setup (#3)
    * add document type
    
    * register hangman model
    
    * add data schema
    
    * add derived data
    
    * add incorrect guess count field
    
    * prepare base data
    
    * add guess method

---
+++ scripts/data-model.js
@@ -0,0 +1,173 @@
+export const GameStatus = Object.freeze({
+  NotStarted: "NotStarted",
+  InProgress: "InProgress",
+  Success: "Success",
+  Fail: "Fail",
+});
+
+export const GameState = Object.freeze([
+  `
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+`,
+  `
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+`,
+  `
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+`,
+  `
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+`,
+  `
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+`,
+  `
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+`,
+  `
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+`,
+]);
+
+const { StringField, SetField, NumberField } = foundry.data.fields;
+
+export class HangmanModel extends foundry.abstract.TypeDataModel {
+  static defineSchema() {
+    return {
+      targetWord: new StringField({
+        blank: false,
+        required: true,
+        label: "Target Word",
+        gmOnly: true,
+        trim: true,
+      }),
+      status: new StringField({
+        blank: false,
+        choices: GameStatus,
+        initial: GameStatus.NotStarted,
+        required: true,
+        label: "Game Status",
+      }),
+      incorrectGuesses: new NumberField({
+        required: true,
+        integer: true,
+        initial: 0,
+        nullable: false,
+      }),
+      guessedCharacters: new SetField(
+        new StringField({
+          blank: false,
+          trim: true,
+        }),
+        {
+          required: true,
+          initial: new Set(),
+          label: "Guessed Characters",
+        }
+      ),
+      guessedWords: new SetField(
+        new StringField({
+          blank: false,
+          trim: true,
+        }),
+        {
+          required: true,
+          initial: new Set(),
+          label: "Guessed Words",
+        }
+      ),
+    };
+  }
+
+  /**
+   * @override
+   */
+  prepareBaseData() {
+    if (this.incorrectGuesses >= GameState.length - 1) {
+      this.status = GameStatus.Fail;
+    } else if (
+      this.guessedWords.has(this.targetWord) ||
+      this.targetWord.split().every((char) => this.guessedCharacters.has(char))
+    ) {
+      this.status = GameStatus.Success;
+    }
+  }
+
+  /**
+   * @override
+   */
+  prepareDerivedData() {
+    this.wordLength = this.targetWord.length;
+    this.display = this.targetWord
+      .split()
+      .map((char) => (this.guessedCharacters.has(char) ? char : "_"));
+  }
+
+  async guess(guess) {
+    const sanitizedGuess = guess.replaceAll(/\W|_|\d/g, "");
+    let correct = false;
+    if (sanitizedGuess.length === 0) {
+      return;
+    } else if (sanitizedGuess.length === 1) {
+      this.guessedCharacters.add(sanitizedGuess);
+      correct = this.targetWord.includes(sanitizedGuess);
+    } else {
+      this.guessedWords.add(sanitizedGuess);
+      correct = this.targetWord === sanitizedGuess;
+    }
+
+    if (!correct) {
+      this.incorrectGuesses += 1;
+    }
+
+    // FIXME should we even await this? or return eagerly with the correct result?
+    await this.parent.update({
+      "system.incorrectGuesses": this.incorrectGuesses,
+      "system.guessedCharacters": this.guessedCharacters,
+      "system.guessedWords": this.guessedWords,
+    });
+    return correct;
+  }
+}
--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,9 @@
-Hooks.once('init', async function() {
+import { HangmanModel } from "./data-model.js";
 
+Hooks.once("init", async function () {
+  Object.assign(CONFIG.JournalEntryPage.dataModels, {
+    "hangman-exe.hangman": HangmanModel,
+  });
 });
 
-Hooks.once('ready', async function() {
-
-});
+Hooks.once("ready", async function () {});
